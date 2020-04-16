d54eb867b8b71dd2921fbb55deed36634bf6fbf1
Fajar001 <codingpena@gmail.com>
2020-04-16 15:29:06 +0700

    update 1.10

--- src/main/java/com/lawencon/app/springbootproject/controller/TrainerController.java
+++ src/main/java/com/lawencon/app/springbootproject/controller/TrainerController.java
@@ -9,11 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lawencon.app.springbootproject.model.Trainer;
 import com.lawencon.app.springbootproject.service.TrainerService;
 
 @RestController
--- src/main/java/com/lawencon/app/springbootproject/service/JawabanService.java
+++ src/main/java/com/lawencon/app/springbootproject/service/JawabanService.java
@@ -3,7 +3,6 @@ package com.lawencon.app.springbootproject.service;
 import java.util.List;
 
 import com.lawencon.app.springbootproject.model.Jawaban;
-import com.lawencon.app.springbootproject.model.Trainer;
 
 public interface JawabanService {
 	abstract List<?> findAll()throws Exception;
@@ -11,6 +10,4 @@ public interface JawabanService {
 	abstract void insert(Jawaban jawaban)throws Exception;
 	abstract Jawaban update(Jawaban jawaban)throws Exception;
 	abstract void delete(Jawaban jawaban)throws Exception;
-	
-//	abstract int getNilaiByStudent(Trainer tr) throws Exception; 
 }

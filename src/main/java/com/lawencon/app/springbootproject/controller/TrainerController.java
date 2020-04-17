322d9ebab0b74354ce2baae37d13bf133516b729
Fajar001 <codingpena@gmail.com>
2020-04-17 09:37:04 +0700

    update TrainerController

--- src/main/java/com/lawencon/app/springbootproject/controller/TrainerController.java
+++ src/main/java/com/lawencon/app/springbootproject/controller/TrainerController.java
@@ -9,19 +9,84 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lawencon.app.springbootproject.model.Trainer;
 import com.lawencon.app.springbootproject.service.TrainerService;
 
 @RestController
 @CrossOrigin("*")
-public class TrainerController {
+@RequestMapping("/trainer")
+public class TrainerController extends BaseController<Trainer>{
 
 	@Autowired
 	private TrainerService trainerService;
 	
-	@GetMapping("/trainerreport/{id}")
-	public ResponseEntity<?> getList(@PathVariable String id){
+	@GetMapping("/list")
+	public ResponseEntity<?> getList(){
+		List<Trainer> listTrainer = new ArrayList<>();
+		try {
+			listTrainer = trainerService.findAll();
+			return new ResponseEntity<>(listTrainer, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(listTrainer, HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping("/search")
+	public ResponseEntity<?> getListId(@RequestBody String content){
+		try {
+			Trainer trainer = readValue(content, Trainer.class);
+			trainerService.findById(trainer);
+			return new ResponseEntity<>("Success", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PostMapping("/insert")
+	public ResponseEntity<?> getInsert(@RequestBody String content){
+		try {
+			Trainer trainer = readValue(content, Trainer.class);
+			trainerService.createTrainer(trainer);
+			return new ResponseEntity<>("Success insert", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Failed insert", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<?> getUpdate(@RequestBody String content){
+		try {
+			Trainer trainer = readValue(content, Trainer.class);
+			trainerService.updateTrainer(trainer);
+			return new ResponseEntity<>("Success update", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Failed update", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@PostMapping("/delete")
+	public ResponseEntity<?> getDelete(@RequestBody String content){
+		try {
+			Trainer trainer = readValue(content, Trainer.class);
+			trainerService.deleteTrainer(trainer);
+			return new ResponseEntity<>("Success delete", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Failed delete", HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping("/report/{id}")
+	public ResponseEntity<?> getListReport(@PathVariable String id){
 		List<?> data = new ArrayList<>();
 		try {
 			data = trainerService.cetakReportTrainer(id);
